6743124eda3eb62297a8f4b3785a3d66fe0f8f94
breyonna <hasty.breyonna@gmail.com>
2023-05-28 00:21:39 -0400

    Form's page now has JavaScript functionality, needs a while loop but mostly done

---
+++ scripts/Form.js
@@ -0,0 +1,44 @@
+class Person{
+    constructor(fullName,email){
+        this.fullName = document.getElementById('fname').value;
+        this.email = document.getElementById('email').value;
+    }
+}
+
+//Selects each button for the forms, both donations button and subscriber button
+let donateButton = document.getElementById('donButton');
+let subscriberButton = document.getElementById('subButton');
+let donMessage = document.getElementById('message-don');
+let subMessage = document.getElementById('message-sub');
+
+    // Event: Get full name and display a message
+    function donate(){ 
+        const personDonate = new Person();
+        let fullName = document.getElementById('fname').value;
+    
+    donateButton.style.display = 'none';
+    donMessage.innerHTML =  `Thank you ${fullName}, We'll be in touch.`;
+        
+            
+            
+        console.log(personDonate);
+        console.log(placeHolderText);
+        console.log(placeHolderText2);
+       
+    };
+    
+    function subscribe(){ 
+        const personSubscribe = new Person();
+        let fullName = document.getElementById('fname').value;
+      
+
+      
+    subscriberButton.style.display = 'none';
+    subMessage.innerHTML =  `Thank you ${fullName}, We'll be in touch.`;
+            
+        console.log(personSubscribe);
+    };
+    
+//Button Event
+donateButton.addEventListener('click',donate);
+subscriberButton.addEventListener('click',subscribe);
