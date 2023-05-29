f2d064147690723ed5e82fdcd82484a979211871
breyonna <hasty.breyonna@gmail.com>
2023-05-28 23:31:07 -0400

    Js functionality added to forms button page

--- scripts/Form.js
+++ scripts/Form.js
@@ -11,34 +11,46 @@ let subscriberButton = document.getElementById('subButton');
 let donMessage = document.getElementById('message-don');
 let subMessage = document.getElementById('message-sub');
 
+
+
     // Event: Get full name and display a message
     function donate(){ 
         const personDonate = new Person();
         let fullName = document.getElementById('fname').value;
-    
-    donateButton.style.display = 'none';
-    donMessage.innerHTML =  `Thank you ${fullName}, We'll be in touch.`;
-        
-            
-            
+   
+
+        for(let i = 0; i < fullName.length; i++){
+            if(fullName.length !== i){
+          
+                donateButton.style.display = 'none';
+                donMessage.innerHTML =  `Thank you ${fullName}, We'll be in touch.`;
+        } 
         console.log(personDonate);
         console.log(placeHolderText);
         console.log(placeHolderText2);
-       
+
+        }
     };
-    
+ 
+
     function subscribe(){ 
         const personSubscribe = new Person();
         let fullName = document.getElementById('fname').value;
-      
 
-      
-    subscriberButton.style.display = 'none';
-    subMessage.innerHTML =  `Thank you ${fullName}, We'll be in touch.`;
-            
+
+        for(let i = 0; i < fullName.length; i++){
+            if(fullName.length !== i){
+
+                subscriberButton.style.display = 'none';
+                subMessage.innerHTML =  `Thank you ${fullName}, We'll be in touch.`;
+            }
         console.log(personSubscribe);
+        }
     };
-    
+
+   
 //Button Event
-donateButton.addEventListener('click',donate);
-subscriberButton.addEventListener('click',subscribe);
+donateButton.addEventListener('click', donate);
+subscriberButton.addEventListener('click', subscribe);
+
+
